15520bd7185cb2f546ad77264c1057944fed79f7
Felix Matschilles <felix@MBP-von-Felix.fritz.box>
2023-02-05 22:05:07 +0100

    add comments to input validation util

--- src/api/ticket-input-validation.util.ts
+++ src/api/ticket-input-validation.util.ts
@@ -6,24 +6,34 @@ const ajv = new Ajv({
   allErrors: true,
 });
 
+/**
+ * Throws an error if the inputCandidate is not a valid TicketInput
+ * Infers the type of inputCandidate to be TicketInput
+ */
 export function isTicketInputValObjOrThrow(
   inputCandidate: any,
 ): asserts inputCandidate is TicketInput {
+  // Validation schema for TicketInput
   const schema: { [k: string]: JSONSchema6Definition } = {
     user_id: { type: 'number' },
     title: { type: 'string', minLength: 1 },
     tags: { type: 'array', items: { type: 'string' }, maxItems: 4 },
   };
 
+  // Validate inputCandidate against schema
   const validate = ajv.compile({
     properties: schema,
     required: ['user_id', 'title'],
+    type: "object",
   });
 
   const isValid = validate(inputCandidate);
+
+  // If the inputCandidate is not valid, throw error
   if (!isValid) {
     const validationErrors = validate.errors || [];
     const firstValidationError = validationErrors[0];
+
     if(firstValidationError){
       throw new Error(
         `Invalid input: ${firstValidationError.instancePath + ": "+ firstValidationError.message}`,
