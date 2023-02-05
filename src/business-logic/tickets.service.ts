1a4113067ef1cf0998b17440beb360171e375b7a
Felix Matschilles <felix@MBP-von-Felix.fritz.box>
2023-02-05 17:21:30 +0100

    wire http repository

--- src/business-logic/tickets.service.ts
+++ src/business-logic/tickets.service.ts
@@ -1,14 +1,29 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TicketInput } from '../model/input/ticket-input';
 import { TicketsRepository } from '../data-access/tickets.repository';
+import { HttpRepository } from 'src/data-access/http.repository';
 
 @Controller()
 export class TicketsService {
   constructor(
     private readonly ticketsRepository: TicketsRepository,
+    private readonly httpRepository: HttpRepository
   ) {}
   
   async processTicketCreation(input: TicketInput){
-    return this.ticketsRepository.createTicket(input);
+    // Create ticket
+    await this.ticketsRepository.createTicket(input);
+
+    // Upsert tags 
+    // TODO
+
+    // Retrieve tag with highest count
+    // TODO
+    // send stats to external service
+    const statsStub = {
+      tag: 'tag',
+      count: 1,
+    }
+    await this.httpRepository.postTagStats(statsStub);
   }
 }
