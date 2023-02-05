0ff68c01898279bf6cfd2b7e9d8fb37282fabbb1
Felix Matschilles <felix@MBP-von-Felix.fritz.box>
2023-02-05 17:14:32 +0100

    rm unused imports

--- src/api/ticket.controller.ts
+++ src/api/ticket.controller.ts
@@ -1,41 +1,37 @@
-import { Body, ConsoleLogger, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
-import Ajv from 'ajv';
-import { Request, Response } from 'express';
-import { JSONSchema6Definition } from 'json-schema';
+import {
+  Body, Controller,
+  Get,
+  Param,
+  Post, Res
+} from '@nestjs/common';
+import { Response } from 'express';
 import { TicketsService } from '../business-logic/tickets.service';
 import { isTicketInputValObjOrThrow } from './ticket-input-validation.util';
 
 @Controller('ticket')
 export class TicketsController {
-  constructor(
-    private readonly ticketsService: TicketsService,
-  ) {}
+  constructor(private readonly ticketsService: TicketsService) {}
 
-  @Get(":user_id")
+  @Get(':user_id')
   async getTicketsByUserId(
     @Res() res: Response,
-    @Param("user_id") user_id: number) {
-   {
+    @Param('user_id') user_id: number,
+  ) {
     return '';
   }
 
   @Post()
-  async postHello(
-    @Res() res: Response,
-    @Body() ticketInput: any) {
+  async postHello(@Res() res: Response, @Body() ticketInput: any) {
     try {
       isTicketInputValObjOrThrow(ticketInput);
     } catch (err: any) {
-
-      // Return 422 
-      console.log(err.message)
-      res.status(422).send(err.message); 
+      // Return 422
+      console.log(err.message);
+      res.status(422).send(err.message);
     }
 
-    await this.ticketsService.processTicketCreation(
-      ticketInput,
-    )
-    
+    await this.ticketsService.processTicketCreation(ticketInput);
+
     res.status(201).send('Ticket created');
   }
 }
