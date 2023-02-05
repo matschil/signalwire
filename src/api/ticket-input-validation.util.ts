d1ed13c6841c416b933f95c8e7c7e8a54898882a
Felix Matschilles <felix@MBP-von-Felix.fritz.box>
2023-02-05 16:31:55 +0100

    add input valdiation

--- src/api/ticket-input-validation.util.ts
+++ src/api/ticket-input-validation.util.ts
@@ -1,4 +1,3 @@
-import { Body, Controller, Get, Injectable, Post } from '@nestjs/common';
 import Ajv from 'ajv';
 import { JSONSchema6Definition } from 'json-schema';
 
@@ -6,18 +5,30 @@ const ajv = new Ajv({
   allErrors: true,
 });
 
-export interface InputValidationSchema {
-  [k: string]: JSONSchema6Definition;
+export function isTicketInputValObjOrThrow(
+  inputCandidate: any,
+): asserts inputCandidate is any {
+  const schema: { [k: string]: JSONSchema6Definition } = {
+    userId: { type: 'number' },
+    title: { type: 'string' },
+    tags: { type: 'array', items: { type: 'string' }, maxLength: 4 },
+  };
+
+  const validate = ajv.compile({
+    properties: schema,
+    required: ['userId', 'title'],
+  });
+
+  const isValid = validate(inputCandidate);
+  if (!isValid) {
+    const validationErrors = validate.errors || [];
+    throw new Error(
+      JSON.stringify({
+        errorPrefix: 'validateTicketInputValObjs',
+        row: inputCandidate,
+        validationErrors,
+        errorText: ajv.errorsText(validationErrors),
+      }),
+    );
+  }
 }
-
-
-export const TicketInputValObjValidationSchema: InputValidationSchema = {
-  userId: { type: 'number' },
-  title: { type: 'string' },
-  tags: { type: 'array', items: { type: 'string' }, maxLength: 4 },
-};
-
-
-
-
-
