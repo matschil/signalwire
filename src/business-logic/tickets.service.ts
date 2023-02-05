c9ff265408e6fca4df38913dce67bc807aedffd1
Felix Matschilles <felix@MBP-von-Felix.fritz.box>
2023-02-05 16:13:56 +0100

    add api tests for input validation

--- src/business-logic/tickets.service.ts
+++ src/business-logic/tickets.service.ts
@@ -2,7 +2,5 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller()
 export class TicketsService {
-  constructor() {}
-
-  
+  constructor() {} 
 }
--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -1,17 +1,103 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import * as supertest from "supertest"
+import * as supertest from 'supertest';
 
-describe('AppController', () => {
+describe('POST /tickets', () => {
+  const request = supertest('');
 
-  const request = supertest("")
+  const serverBaseUrl = 'http://localhost:3000';
+  const ticketRoute = '/tickets';
+  
+  
+  it('[Input Validation] should return response code 422 for invalid inputs', async () => {
+    const invalidInputs = [
+      // id is not a number
+      {
+        user_id: '1234',
+        title: 'My title',
+        tags: ['tag1', 'tag2'],
+      },
+      // title is not a string
+      {
+        user_id: '1234',
+        title: 1234,
+        tags: ['tag1', 'tag2'],
+      },
+      // user_id is null
+      {
+        user_id: null,
+        title: 1234,
+        tags: ['tag1', 'tag2'],
+      },
+      // user_id is undefined
+      {
+        user_id: undefined,
+        title: 1234,
+        tags: ['tag1', 'tag2'],
+      },
+      // title is null
+      {
+        user_id: 1234,
+        title: null,
+        tags: ['tag1', 'tag2'],
+      },
+      // title is undefined
+      {
+        user_id: 1234,
+        title: undefined,
+        tags: ['tag1', 'tag2'],
+      },
+      // title is empty string
+      {
+        user_id: 1234,
+        title: '',
+        tags: ['tag1', 'tag2'],
+      },
+      // tags is not an array
+      {
+        user_id: 1234,
+        title: 'My title',
+        tags: 'tag1',
+      },
+      // tags contains more than 4 elements
+      {
+        user_id: 1234,
+        title: 'My title',
+        tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5'],
+      },
+    ];
 
-  beforeEach(async () => {
+    for (const invalidInput of invalidInputs) {
+      const res = await request.post(`${serverBaseUrl}${ticketRoute}`).send(invalidInput);
+      expect(res.status).toBe(422);
+    }
   });
 
-  describe('root', () => {
-    it("Gan GET stub data", async () => {
-      const res =  await request.get("http://localhost:3000")
-      expect(res.status).toBe(200)
-    })
+  it('[Input Validation] should return response code 200 for valid inputs', async () => {
+    const invalidInputs = [
+      {
+        user_id: 1234,
+        title: 'My title',
+        tags: ['tag1', 'tag2'],
+      },
+      {
+        user_id: 1234,
+        title: 'My title',
+        tags: []
+      },
+      {
+        user_id: 1234,
+        title: 'My title',
+      },
+      {
+        user_id: 1234,
+        title: 'My title',
+        tags: ['tag1', 'tag2', 'tag3', 'tag4'],
+      },
+    ];
+
+    for (const invalidInput of invalidInputs) {
+      const res = await request.post(`${serverBaseUrl}${ticketRoute}`).send(invalidInput);
+      expect(res.status).toBe(422);
+    }
   });
 });
